a4d77e55e4fe99894e5e5f89d2534c8f3a900b7b
Srijan Srivastav <srijan.srivastav.personal@gmail.com>
2024-01-02 20:14:34 +0530

    Spit out the logged in principal info through a REST endpoint

--- src/main/java/com/aipainter/authenticationservice/controllers/AuthenticationController.java
+++ src/main/java/com/aipainter/authenticationservice/controllers/AuthenticationController.java
@@ -1,24 +1,19 @@
 package com.aipainter.authenticationservice.controllers;
 
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthenticationController {
-
-    @GetMapping("/login")
-    public String login(OAuth2AuthenticationToken authentication) {
-    System.out.println(authentication);
-        return "Trying to log in..";
-    }
-
-    @GetMapping("/orders")
-    public String orders() {
-        return "Orders page...";
-    }
     @GetMapping("/home")
     public String homePage() {
         return "Hello World!";
+    }
+
+    @GetMapping("/user")
+    public OidcUser userInfo(@AuthenticationPrincipal OidcUser principal) {
+        return principal;
     }
 }
